dfde16a0eeb3441d288d0865c468388bff8bef92
Ángel Jurado Herruzo <rjurado@alumnat.copernic.cat>
2026-04-13 11:46:24 +0200

    Projecte Java Creat

---
+++ Backend/src/main/java/cat/copernic/easytraza/EasytrazaApplication.java
@@ -0,0 +1,13 @@
+package cat.copernic.easytraza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasytrazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasytrazaApplication.class, args);
+	}
+
+}
