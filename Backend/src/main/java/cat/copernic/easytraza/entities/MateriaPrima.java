5fc15b954b032dc5e86f341560f70fcec700ccd1
Ángel Jurado Herruzo <rjurado@alumnat.copernic.cat>
2026-04-15 09:11:12 +0200

    Entitat i Repo de materies primeres creades. #53

---
+++ Backend/src/main/java/cat/copernic/easytraza/entities/MateriaPrima.java
@@ -0,0 +1,50 @@
+package cat.copernic.easytraza.entities;
+
+import jakarta.persistence.*;
+
+/**
+ * ENTITAT MATÈRIA PRIMERA
+ *
+ * Representa una matèria primera del sistema.
+ * És un producte base utilitzat per a la fabricació de producte acabat.
+ *
+ * @author Ángel Jurado
+ */
+@Entity
+@Table(name = "materies_primeres")
+public class MateriaPrima {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nomMateria;
+
+    @Column(length = 150)
+    private String descripcio;
+
+
+    /*********************       .CONSTRUCTORS.       *********************/
+    public MateriaPrima() { }
+
+    public MateriaPrima(Long id, String nomMateria, String descripcio) {
+        this.id = id;
+        this.nomMateria = nomMateria;
+        this.descripcio = descripcio;
+    }
+
+
+    /*********************       .GETTERS & SETTERS.       *********************/
+    // ─────────── ID ───────────
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    // ─────────── NOM ───────────
+    public String getNomMateria() { return nomMateria; }
+    public void setNomMateria(String nomMateria) { this.nomMateria = nomMateria; }
+
+    // ─────── DESCRIPCIO ───────
+    public String getDescripcio() { return descripcio; }
+    public void setDescripcio(String descripcio) { this.descripcio = descripcio; }
+}
---
+++ Backend/src/main/java/cat/copernic/easytraza/repository/MateriaPrimaRepository.java
@@ -0,0 +1,6 @@
+package cat.copernic.easytraza.repository;
+
+import cat.copernic.easytraza.entities.MateriaPrima;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MateriaPrimaRepository extends JpaRepository<MateriaPrima, Long> {}
