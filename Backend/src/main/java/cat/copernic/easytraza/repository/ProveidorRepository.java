5a6d9eab8a4d6d2b6b40debbe7cc080a78949054
Ángel Jurado Herruzo <rjurado@alumnat.copernic.cat>
2026-04-15 09:38:38 +0200

    Entitat i Repo de proveïdors creades. #55

---
+++ Backend/src/main/java/cat/copernic/easytraza/repository/ProveidorRepository.java
@@ -0,0 +1,6 @@
+package cat.copernic.easytraza.repository;
+
+import cat.copernic.easytraza.entities.Proveidor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProveidorRepository extends JpaRepository<Proveidor, Long> {}
